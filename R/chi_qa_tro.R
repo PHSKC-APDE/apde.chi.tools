7130f65db80f38abe2f9c33aafc74a0441601e9c
rwbuie <rwbuie@gmail.com>
2024-06-28 21:08:55 -0700

    mid process of modifying to silence versus verbosity. adding return/exit logic. rename function, created updated man info

--- R/chi_qa_tro.R
+++ R/chi_qa_tro.R
@@ -1,26 +1,23 @@
-#' QA for CHI/Tableau ready standards using R.
+#'
 #'
 #' @description
-#' This functions seeks to ensure that the data being assessed meet the detailed CHI
-#' Tableau Ready standards detailed on our network drive:
-#' "//phshare01/epe_share/WORK/CHI Visualizations/Tableau Ready Output Format_v2.xlsx"
+#' Checks data for compliance with CHI TRO standard
+
 #'
 #' @details
-#' This function ensures that the structure of the data matches all CHI Tableau Ready
-#' specifications. QA for data quality vis-à-vis previous production data, CHAT, or any other source,
-#' must be performed separately
-#'
+#' This function tests if the structure of the data matches CHI Tableau Ready Output
+#' specifications. If set to verbose mode (default) will report diagnostic information in
+#' The user interface as warnings (if failed) and messages (for progress and pass). In any
+#' Case will return 1 or 0 for pass or fail respectively.
+#' The CHI Tableau Ready Output (TRO) standards can be reviewed [here]("//phshare01/epe_share/WORK/CHI Visualizations/Tableau Ready Output Format_v2.xlsx")
 #'
 #' @param chi_est Name of a data.table or data.frame containing the prepared data to be pushed to SQL
 #' @param chi_meta Name of a data.table or data.frame containing the metadata to be pushed to SQL
 #' @param acs Logical. Indicates whether it is ACS data (which does not have / need varnames)
 #' @param ignore_trends Logical. Indicates whether the time_trends column should be ignored when checking for missing data.
-#' @param verbose Logical. Should the function be talkative?
+#' @param verbose default TRUE, if false will only return status
 #'
-#' @return If there are no problems, a printed statement of success. Otherwise, it will stop and provide informative
-#' feedback every time there is an error.
-#'
-#' @export
+#' @return 1 or 0 for pass or fail status
 #'
 #' @keywords CHI, Tableau, Production
 #'
@@ -36,126 +33,199 @@
 #'
 #' # run function
 #' }
+#'
+#' @export
 
 # chi_qa function ----
 
-chi_qa <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends = T, verbose = FALSE){
+chi_qa_tro <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends = T, verbose = FALSE){
+  status <- 1
 
   #global variables used by data.table declared as NULL here to play nice with devtools::check()
-    indicator_key <- result_type <- result <- upper_bound <- lower_bound <- rse <- caution <- tab <- suppression <- time_trends <- NULL
+  indicator_key <- result_type <- result <- upper_bound <- lower_bound <- rse <- caution <- tab <- suppression <- time_trends <- NULL
 
-  # Check that both the results and the metadata were provided ----
-    if(is.null(chi_est)){
-      stop("You must provide the name of a data.frame or data.table that contains the CHI results (e.g., chi_est = chi_2018")
+  if(verbose){
+    message("Checking that both the results and the metadata were provided")
+  }
+  if(is.null(chi_est)){
+    status <- 0
+    if(verbose) {
+      warning("You must provide the name of a data.frame or data.table that contains the CHI results.")
     }
 
-    if(is.null(chi_meta)){
-      stop("You must provide the name of a data.frame or data.table that contains the CHI metadata (e.g., chi_meta = chi_meta_2018")
+  }
+  if(is.null(chi_meta)){
+    status <- 0
+    if(verbose){
+      warning("You must provide the name of a data.frame or data.table that contains the CHI metadata ")
     }
 
-    chi_est <- data.table::setDT(copy(chi_est))
-    chi_meta <- data.table::setDT(copy(chi_meta))
+  }
+  #if both data sets are not provided, abort check
+  if(status == 0) {
+    if(verbose){
+      warning("Check incomplete. Please correct errors to proceed")
+    }
+    return(status)
+  }
+
+  chi_est <- data.table::setDT(copy(chi_est))
+  chi_meta <- data.table::setDT(copy(chi_meta))
 
   ## Load reference YAML ----
-    chi.yaml <- yaml::yaml.load(httr::GET(url = "https://raw.githubusercontent.com/PHSKC-APDE/rads/main/ref/chi_qa.yaml", httr::authenticate(Sys.getenv("GITHUB_TOKEN"), "")))
-
+  chi.yaml <- yaml::yaml.load(httr::GET(url = "https://raw.githubusercontent.com/PHSKC-APDE/rads/main/ref/chi_qa.yaml", httr::authenticate(Sys.getenv("GITHUB_TOKEN"), "")))
 
   ## Check columns ----
-      # Confirm that all column names are unique ----
-          if(length(names(chi_est)) != length(unique(names(chi_est)))) {
-            stop("You submitted a dataset where at least two columns have the same name.
-                 All names in chi_est must be unique.")
-          }
+  if(verbose) {
+    message("Checking that all column names are unique")
+  }
+  if(length(names(chi_est)) != length(unique(names(chi_est)))) {
+    status <- 0
+    if(verbose){
+      warning("You submitted a dataset where at least two columns have the same name. All names in chi_est must be unique.")
+    }
+  }
+  if(length(names(chi_meta)) != length(unique(names(chi_meta)))) {
+    status <- 0
+    if(verbose){
+      warning("You submitted a metadata table where at least two columns have the same name. All names in chi_meta must be unique.")
+    }
+  }
 
-          if(length(names(chi_meta)) != length(unique(names(chi_meta)))) {
-            stop("You submitted a metadata table where at least two columns have the same name.
-                       All names in chi_meta must be unique.")
-          }
+  if(verbose){
+    message("Checking that all necessary columns exist")
+  }
+  missing.var <- setdiff(names(chi.yaml$vars), names(chi_est))
+  if(length(missing.var) > 0){
+    status <- 0
+    if(verbose) {
+      missing.var <- paste(missing.var, collapse = ", ")
+      warning(glue::glue("You are missing the following critical columns(s) in chi_est: {missing.var}"))
+    }
+  }
 
-      # Confirm all necessary columns exist ----
-          missing.var <- setdiff(names(chi.yaml$vars), names(chi_est))
-          if(length(missing.var) > 0){
-            missing.var <- paste(missing.var, collapse = ", ")
-            stop(glue::glue("You are missing the following critical columns(s) in chi_est: {missing.var}"))
-          }
+  missing.var <- setdiff(names(chi.yaml$metadata), names(chi_meta))
+  if(length(missing.var) > 0){
+    status <- 0
+    if(verbose){
+      missing.var <- paste(missing.var, collapse = ", ")
+      warning(glue::glue("You are missing the following critical columns(s) in chi_meta: {missing.var}"))
+    }
+  }
 
-          missing.var <- setdiff(names(chi.yaml$metadata), names(chi_meta))
-          if(length(missing.var) > 0){
-            missing.var <- paste(missing.var, collapse = ", ")
-            stop(glue::glue("You are missing the following critical columns(s) in chi_meta: {missing.var}"))
-          }
-
-      # Confirm that there are no additional variables ----
-          extra.var <- setdiff(names(chi_est), names(chi.yaml$vars))
-          if(length(extra.var) > 0){
-            extra.var <- paste(extra.var, collapse = ", ")
-            stop(glue::glue("Your dataset contains the following columns that are not CHI compliant: {extra.var}.
+  # Confirm that there are no additional variables ----
+  extra.var <- setdiff(names(chi_est), names(chi.yaml$vars))
+  if(length(extra.var) > 0){
+    status <- 0
+    if(verbose){
+      extra.var <- paste(extra.var, collapse = ", ")
+      warning(glue::glue("Your dataset contains the following columns that are not CHI compliant: {extra.var}.
                             Please drop these variables from chi_est before attempting to QA the data again."))
-          }
+    }
+  }
 
-          extra.var <- setdiff(names(chi_meta), names(chi.yaml$meta))
-          if(length(extra.var) > 0){
-            extra.var <- paste(extra.var, collapse = ", ")
-            stop(glue::glue("Your metadata table contains the following columns that are not CHI compliant: {extra.var}.
+  extra.var <- setdiff(names(chi_meta), names(chi.yaml$meta))
+  if(length(extra.var) > 0){
+    status <- 0
+    if(verbose){
+      extra.var <- paste(extra.var, collapse = ", ")
+      warning(glue::glue("Your metadata table contains the following columns that are not CHI compliant: {extra.var}.
                             Please drop these variables from chi_meta before attempting to QA the data again."))
-          }
+    }
+  }
 
-  ## Confirm that variables are of the proper class ----
-          if(verbose) message("Validating CHI estimates: ")
-          validate_yaml_data(DF = chi_est, YML = chi.yaml, VARS = "vars") # check CHI estimate table
+  if(verbose){
+    message("Confirm that variables are of the proper class")
+  }
+  if(verbose){
+    message("Validating CHI estimates: ")
+  }
+  rads::validate_yaml_data(DF = chi_est, YML = chi.yaml, VARS = "vars") # check CHI estimate table
+  if(verbose) message(paste("", "Validating CHI metadata: ", sep = "\n"))
+  rads::validate_yaml_data(DF = chi_meta, YML = chi.yaml, VARS = "metadata") # check CHI metadata table
+  if(verbose) message(paste("", "", sep = "\n"))
 
-          if(verbose) message(paste("", "Validating CHI metadata: ", sep = "\n"))
-          validate_yaml_data(DF = chi_meta, YML = chi.yaml, VARS = "metadata") # check CHI metadata table
+  if(verbose){
+    message("Confirm that critical columns are not missing any values")
+  }
+  for(mycol in c("indicator_key", "year", "data_source", "tab", "cat1", "cat1_group", "run_date")){
+    if(nrow(chi_est[is.na(get(mycol))]) > 0){
+      status <- 0
+      warning(paste0("'", mycol, "' is missing in at least one row but is a critical identifier column in CHI data. \n", "Fix the error and run this QA script again."))
+    }
+  }
 
-          if(verbose) message(paste("", "", sep = "\n"))
+  for(mycol in c("result", "lower_bound", "upper_bound", "se", "rse", "numerator", "denominator", "chi", "source_date", "run_date", "comparison_with_kc")){
+    if(nrow(chi_est[is.na(get(mycol)) & is.na(suppression)]) > 0){
+      status <- 0
+      warning(paste0("\U00026A0 Warning: '", mycol, "' is missing in at least one row of the CHI data."))
+    }
+  }
 
-  ## Confirm that critical columns are not missing any values ----
-          for(mycol in c("indicator_key", "year", "data_source", "tab", "cat1", "cat1_group", "run_date")){
-            if(nrow(chi_est[is.na(get(mycol))]) > 0){stop(paste0("'", mycol, "' is missing in at least one row but is a critical identifier column in CHI data. \n",
-                                                            "Fix the error and run this QA script again."))}
-          }
+  for(mycol in setdiff(names(chi.yaml$metadata), c("latest_year_kc_pop", "latest_year_count"))){
+    if(nrow(chi_meta[is.na(get(mycol))]) > 0){
+      status <- 0
+      warning(paste0("'", mycol, "' is missing in at least one row but is a critical identifier column in CHI metadata. \n", "Fix the error and run this QA script again."))
+    }
+  }
 
-          for(mycol in c("result", "lower_bound", "upper_bound", "se", "rse", "numerator", "denominator", "chi", "source_date", "run_date", "comparison_with_kc")){
-            if(nrow(chi_est[is.na(get(mycol)) & is.na(suppression)]) > 0){message(paste0("\U00026A0 Warning: '", mycol, "' is missing in at least one row of the CHI data."))}
-          }
+  for(mycol in c("latest_year_kc_pop", "latest_year_count")){
+    if(nrow(chi_meta[is.na(get(mycol))]) > 0){
+      status <- 0
+      warning(paste0("\U00026A0 Warning: '", mycol, "' is missing in at least one row of the metadata."))
+    }
+  }
 
-          for(mycol in setdiff(names(chi.yaml$metadata), c("latest_year_kc_pop", "latest_year_count"))){
-            if(nrow(chi_meta[is.na(get(mycol))]) > 0){stop(paste0("'", mycol, "' is missing in at least one row but is a critical identifier column in CHI metadata. \n",
-                                                                 "Fix the error and run this QA script again."))}
-          }
-
-          for(mycol in c("latest_year_kc_pop", "latest_year_count")){
-            if(nrow(chi_meta[is.na(get(mycol))]) > 0){message(paste0("\U00026A0 Warning: '", mycol, "' is missing in at least one row of the metadata."))}
-          }
+  if(status == 0) {
+    if(verbose) {
+      warning("Check incomplete. Please correct errors to proceed")
+    }
+    return(status)
+  }
 
   ## Set the columns in standard order ----
-        setcolorder(chi_est, names(chi.yaml$vars))
-        setcolorder(chi_meta, names(chi.yaml$meta))
+  setcolorder(chi_est, names(chi.yaml$vars))
+  setcolorder(chi_meta, names(chi.yaml$meta))
 
-  ## Basic logic checks for estimates ----
-      # Check for infinite values, which cannot be pushed to SQL ----
-        for(var in c("result", "lower_bound", "upper_bound", "se", "rse",
-                     "numerator", "denominator")){
-          if(nrow(chi_est[is.infinite(get(var))]) > 0 ){
-            stop(glue::glue("There is at least one row where is.infinite({var}) == T.
-                     Please fix this problem before rerunning chi_qa() (e.g., by setting it equal to NA)
-                     You can view the problematic data by typing something like: View(chi_est[is.infinite({var}), ])"))
-          }
-        }
+  #Basic logic checks for estimates
 
-      # proportions should always be between zero and one ----
-        chi_est <- merge(chi_est, chi_meta[, list(indicator_key, result_type)], by = "indicator_key", all.x = TRUE, all.y = FALSE) # merge on result_type
-        if(nrow(chi_est[result_type=="proportion" & !result %between% c(0, 1)]) > 0){
-          stop("There is at least one row where where the metadata states that the indicator is a proportion but the result is outside [0,1].
-               Please fix either the metadata table or the CHI estimates and try again.")
-        }
+  if(verbose){
+    message("Checking for infinite values, which cannot be pushed to SQL")
+  }
+  for(var in c("result", "lower_bound", "upper_bound", "se", "rse", "numerator", "denominator")){
+    if(nrow(chi_est[is.infinite(get(var))]) > 0 ){
+      status <- 0
+      if(verbose){
+          warning(glue::glue("There is at least one row where is.infinite({var}) == T.
+                       Please fix this problem before rerunning chi_qa() (e.g., by setting it equal to NA)
+                       You can view the problematic data by typing something like: View(chi_est[is.infinite({var}), ])"))
+      }
+    }
+  }
 
-      # upper_bound should be greater than lower_bound ----
-        if(nrow(chi_est[upper_bound < lower_bound, ])){
-          stop("There is at least one row where the upper_bound is less than the lower_bound.
-               Please fix this error prior to re-running the chi_qa() function.
-               You can view the problematic data by typing something like: View(chi_est[upper_bound < lower_bound, ])")
-        }
+  if(verbose){
+    message("checking that proportions are between zero and one")
+  }
+  chi_est <- merge(chi_est, chi_meta[, list(indicator_key, result_type)], by = "indicator_key", all.x = TRUE, all.y = FALSE) # merge on result_type
+  if(nrow(chi_est[result_type=="proportion" & !result %between% c(0, 1)]) > 0){
+    status <- 0
+    if(verbose){
+      warning("There is at least one row where where the metadata states that the indicator is a proportion but the result is outside [0,1].
+                 Please fix either the metadata table or the CHI estimates and try again.")
+    }
+  }
+
+  if(verbose){
+    message("checking if upper_bound is greater than lower_bound")
+  }
+  if(nrow(chi_est[upper_bound < lower_bound, ])){
+    status <- 0
+    if(verbose){
+      warning("There is at least one row where the upper_bound is less than the lower_bound.
+                 Please fix this error prior to re-running the chi_qa() function.
+                 You can view the problematic data by typing something like: View(chi_est[upper_bound < lower_bound, ])")
+    }
+  }
 
       # result should be less than or equal to the upper bound ----
         if(nrow(chi_est[!(result <= upper_bound)])){
@@ -268,8 +338,13 @@ chi_qa <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends = T, 
   ## Check that time_trends are always provided when tab=="trends" ----
         if(ignore_trends == F){
           if(nrow(chi_est[tab == "trends" & is.na(time_trends)]) > 0 ){
-            stop(glue::glue("There is at least one row where tab=='trends' & where 'time_trends' is missing.
-                            Please fill in the missing value before rerunning chi_qa()"))}
+            if(verbose) {
+              message(glue::glue("There is at least one row where tab=='trends' & where 'time_trends' is missing.
+                            Please fill in the missing value before rerunning chi_qa()"))
+            } else {
+              return(0)
+            }
+          }
         }
 
   ## Compare with previous year's results (FOR FUTURE???)----
@@ -285,9 +360,14 @@ chi_qa <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends = T, 
       # actual comparison code should be the same as when comparing to a previous year, so write a small funcion to do this
 
   ## Print success statement!!!!!!!! ####
-    if(verbose) message(paste("\n\U0001f389Congratulations!\U0001f973\n",
+    if(verbose) {
+      message(paste("\n\U0001f389Congratulations!\U0001f973\n",
           "Your data has passed all CHI Tableau Ready formatting, style, and logic checks.\n",
           "However, you are encouraged to read any warnings and consider whether you need to revise the data."))
 
-} # close function
+    }
+
+    return(status)
+
+}
 
