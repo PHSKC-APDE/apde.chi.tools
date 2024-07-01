b6b084e6b35128a4c6b9601c87a9bb15607f1235
rwbuie <rwbuie@gmail.com>
2024-07-01 09:49:57 -0700

    completed package format for chi_qa_tro with option for silen mode

--- R/chi_qa_tro.R
+++ R/chi_qa_tro.R
@@ -13,8 +13,8 @@
 #'
 #' @param chi_est Name of a data.table or data.frame containing the prepared data to be pushed to SQL
 #' @param chi_meta Name of a data.table or data.frame containing the metadata to be pushed to SQL
-#' @param acs Logical. Indicates whether it is ACS data (which does not have / need varnames)
-#' @param ignore_trends Logical. Indicates whether the time_trends column should be ignored when checking for missing data.
+#' @param acs default FALSE, Indicates whether it is ACS data (which does not have / need varnames)
+#' @param ignore_trends default TRUE, Indicates whether the time_trends column should be ignored when checking for missing data.
 #' @param verbose default TRUE, if false will only return status
 #'
 #' @return 1 or 0 for pass or fail status
@@ -36,9 +36,7 @@
 #'
 #' @export
 
-# chi_qa function ----
-
-chi_qa_tro <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends = T, verbose = FALSE){
+chi_qa_tro <- function(chi_est, chi_meta, acs = F, ignore_trends = T, verbose = F){
   status <- 1
 
   #global variables used by data.table declared as NULL here to play nice with devtools::check()
@@ -135,18 +133,20 @@ chi_qa_tro <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends =
   }
 
   if(verbose){
-    message("Confirm that variables are of the proper class")
+    message("Checking that variables are of the proper class")
   }
   if(verbose){
     message("Validating CHI estimates: ")
   }
   rads::validate_yaml_data(DF = chi_est, YML = chi.yaml, VARS = "vars") # check CHI estimate table
-  if(verbose) message(paste("", "Validating CHI metadata: ", sep = "\n"))
+  if(verbose){
+    message(paste("", "Validating CHI metadata: ", sep = "\n"))
+  }
   rads::validate_yaml_data(DF = chi_meta, YML = chi.yaml, VARS = "metadata") # check CHI metadata table
   if(verbose) message(paste("", "", sep = "\n"))
 
   if(verbose){
-    message("Confirm that critical columns are not missing any values")
+    message("Checking that critical columns are not missing any values")
   }
   for(mycol in c("indicator_key", "year", "data_source", "tab", "cat1", "cat1_group", "run_date")){
     if(nrow(chi_est[is.na(get(mycol))]) > 0){
@@ -154,21 +154,18 @@ chi_qa_tro <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends =
       warning(paste0("'", mycol, "' is missing in at least one row but is a critical identifier column in CHI data. \n", "Fix the error and run this QA script again."))
     }
   }
-
   for(mycol in c("result", "lower_bound", "upper_bound", "se", "rse", "numerator", "denominator", "chi", "source_date", "run_date", "comparison_with_kc")){
     if(nrow(chi_est[is.na(get(mycol)) & is.na(suppression)]) > 0){
       status <- 0
       warning(paste0("\U00026A0 Warning: '", mycol, "' is missing in at least one row of the CHI data."))
     }
   }
-
   for(mycol in setdiff(names(chi.yaml$metadata), c("latest_year_kc_pop", "latest_year_count"))){
     if(nrow(chi_meta[is.na(get(mycol))]) > 0){
       status <- 0
       warning(paste0("'", mycol, "' is missing in at least one row but is a critical identifier column in CHI metadata. \n", "Fix the error and run this QA script again."))
     }
   }
-
   for(mycol in c("latest_year_kc_pop", "latest_year_count")){
     if(nrow(chi_meta[is.na(get(mycol))]) > 0){
       status <- 0
@@ -178,7 +175,7 @@ chi_qa_tro <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends =
 
   if(status == 0) {
     if(verbose) {
-      warning("Check incomplete. Please correct errors to proceed")
+      warning("Check incomplete. Please correct errors and try again")
     }
     return(status)
   }
@@ -204,7 +201,7 @@ chi_qa_tro <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends =
   }
 
   if(verbose){
-    message("checking that proportions are between zero and one")
+    message("Checking that proportions are between zero and one")
   }
   chi_est <- merge(chi_est, chi_meta[, list(indicator_key, result_type)], by = "indicator_key", all.x = TRUE, all.y = FALSE) # merge on result_type
   if(nrow(chi_est[result_type=="proportion" & !result %between% c(0, 1)]) > 0){
@@ -216,7 +213,7 @@ chi_qa_tro <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends =
   }
 
   if(verbose){
-    message("checking if upper_bound is greater than lower_bound")
+    message("Checking if upper_bound is greater than lower_bound")
   }
   if(nrow(chi_est[upper_bound < lower_bound, ])){
     status <- 0
@@ -227,125 +224,193 @@ chi_qa_tro <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends =
     }
   }
 
-      # result should be less than or equal to the upper bound ----
-        if(nrow(chi_est[!(result <= upper_bound)])){
-          stop("There is at least one row where the result is not less than or equal to the upper_bound.
+  if(verbose){
+    message("Checking that result is less than or equal to the upper bound")
+  }
+  if(nrow(chi_est[!(result <= upper_bound)])){
+    status <- 0
+    if(verbose){
+      warning("There is at least one row where the result is not less than or equal to the upper_bound.
                Please fix this error prior to rerunning the chi_qa() function.
                You can view the problematic data by typing something like: View(chi_est[!(result <= upper_bound)])")
-        }
+    }
+  }
 
-      # result should be greater than or equal to the lower_bound ----
-        if(nrow(chi_est[!(result >= lower_bound)])){
-          stop("There is at least one row where the result is not greater than or equal to the lower_bound.
+  if(verbose){
+    message("Checking that result is greater than or equal to the lower_bound")
+  }
+  if(nrow(chi_est[!(result >= lower_bound)])){
+    status <- 0
+    if(verbose){
+      warning("There is at least one row where the result is not greater than or equal to the lower_bound.
            Please fix this error prior to rerunning the chi_qa() function.
            You can view the problematic data by typing something like: View(chi_est[!(result >= lower_bound)])")
-        }
-
-      # lower_bound should never be less than zero ----
-        if(nrow(chi_est[lower_bound < 0])){
-          stop("There is at least one row where the lower_bound is less than zero (i.e., it is negative).
+    }
+  }
+  if(verbose){
+    message("Checking that lower_bound is not less than zero")
+  }
+  if(nrow(chi_est[lower_bound < 0])){
+    status <- 0
+    if(verbose){
+          warning("There is at least one row where the lower_bound is less than zero (i.e., it is negative).
            Please fix this error prior to rerunning the chi_qa() function.
           You can view the problematic data by typing something like: View(chi_est[lower_bound < 0])")
-        }
+    }
+  }
 
-      # RSE should always be between 0 and 100 ----
-          # confirmed with Abby 2/7/2020 that want RSE * 100
-          if(nrow(chi_est[!rse %between% c(0, 100)]) > 0 ){
-            message(paste("\U00026A0 Warning: There is at least one row where the RSE (relative standard error) is outside the range of (0, 100].",
+  if(verbose){
+    message("Checking that RSE is between 0 and 100")
+  }
+  # confirmed with Abby 2/7/2020 that want RSE * 100
+  if(nrow(chi_est[!rse %between% c(0, 100)]) > 0 ){
+    status <- 0
+    if(verbose){
+      warning(paste("There is at least one row where the RSE (relative standard error) is outside the range of (0, 100].",
                  "This is not necessarily an error, but you should examine the data to make sure it makes sense.",
                  "You can view the data in question by typing something like: View(chi_est[!rse %between% c(0, 100)])", sep = "\n"))
-          }
+    }
+  }
 
-      # RSE should be on scale of 0-100 (i.e., the proportion should have been multiplied by 100) ----
-          if(nrow(chi_est[!is.na(rse)]) == nrow(chi_est[rse <=1])){
-            stop("All RSEs are within the range (0, 1]. CHI Tableau Ready standards necessitate that these proportions
+  if(verbose){
+    message("Checking that RSE is on scale of 0-100 (i.e., the proportion should have been multiplied by 100)")
+  }
+  if(nrow(chi_est[!is.na(rse)]) == nrow(chi_est[rse <=1])){
+    status <- 0
+    if(verbose){
+      warning("All RSEs are within the range (0, 1]. CHI Tableau Ready standards necessitate that these proportions
                be mutliplied by 100. I.e., .12345 >> 12.345
                Please fix this error prior to rerunning the chi_qa() function.")
-          }
+    }
+  }
 
-      # Caution flag should be toggled if RSE >= 30% ----
-          if(nrow(chi_est[rse>=30 & (caution != "!" | is.na(caution)) ]) > 0 ){
-            stop("There is at least one row where a caution flag ('!') is not used and rse >= 30% or is.na(rse) == T.
+  if(verbose){
+    message("Checking that caution flag exists if RSE >= 30%")
+  }
+  if(nrow(chi_est[rse>=30 & (caution != "!" | is.na(caution)) ]) > 0 ){
+    status <- 0
+    if(verbose){
+      warning("There is at least one row where a caution flag ('!') is not used and rse >= 30% or is.na(rse) == T.
                  Please fix this error prior to rerunning the chi_qa() function.
                  You can view the problematic data by typing something like: View(chi_est[(rse>=30 | is.na(rse)) & (caution != '!' | is.na(caution))])")
-          }
+    }
+  }
 
-  ## Ensure proper rounding ----
-      # result should be to three digits ----
-          if(sum(chi_est$result != round2(chi_est$result, 3), na.rm = T) != 0) {
-            stop("The 'result' column does not appear to be rounded to 3 digits, as specified in the CHI standards")
-          }
+  if(verbose){
+    message("Checking for proper rounding")
+  }
+  if(verbose){
+    message("Checking that result is rounded to three digits")
+  }
+  if(sum(chi_est$result != round2(chi_est$result, 3), na.rm = T) != 0) {
+    status <- 0
+    if(verbose){
+      warning("The 'result' column does not appear to be rounded to 3 digits, as specified in the CHI standards")
+    }
+  }
+  if(verbose){
+    message("Checking that lower_bound is rounded to three digits")
+  }
+  if(sum(chi_est$lower_bound != round2(chi_est$lower_bound, 3), na.rm = T) != 0) {
+    status <- 0
+    if(verbose){
+      warning("The 'lower_bound' column does not appear to be rounded to 3 digits, as specified in the CHI standards")
+    }
+  }
+  if(verbose){
+    message("checking that upper_bound is rounded to three digits")
+  }
+  if(sum(chi_est$upper_bound != round2(chi_est$upper_bound, 3), na.rm = T) != 0) {
+    status <- 0
+    if(verbose){
+      warning("The 'upper_bound' column does not appear to be rounded to 3 digits, as specified in the CHI standards")
+    }
+  }
+  if(verbose){
+    message("Checking that rse is rounded to three digits")
+  }
+  if(sum(chi_est$rse != round2(chi_est$rse, 3), na.rm = T) != 0) {
+    status <- 0
+    if(verbose){
+      warning("The 'rse' column does not appear to be rounded to 3 digits, as specified in the CHI standards")
+    }
+  }
 
-      # lower_bound should be to three digits ----
-          if(sum(chi_est$lower_bound != round2(chi_est$lower_bound, 3), na.rm = T) != 0) {
-            stop("The 'lower_bound' column does not appear to be rounded to 3 digits, as specified in the CHI standards")
-          }
+  if(vebrose){
+    message("Checking that se is rounded to four digits")
+  }
+  if(sum(chi_est$se != round2(chi_est$se, 4), na.rm = T) != 0) {
+    status <- 0
+    if(verbose){
+      warning("The 'se' column does not appear to be rounded to 3 digits, as specified in the CHI standards")
+    }
+  }
 
-      # upper_bound should be to three digits ----
-          if(sum(chi_est$upper_bound != round2(chi_est$upper_bound, 3), na.rm = T) != 0) {
-            stop("The 'upper_bound' column does not appear to be rounded to 3 digits, as specified in the CHI standards")
-          }
-
-      # rse should be to three digits ----
-          if(sum(chi_est$rse != round2(chi_est$rse, 3), na.rm = T) != 0) {
-            stop("The 'rse' column does not appear to be rounded to 3 digits, as specified in the CHI standards")
-          }
-
-      # se should be rounded to four digits ----
-          if(sum(chi_est$se != round2(chi_est$se, 4), na.rm = T) != 0) {
-            stop("The 'se' column does not appear to be rounded to 3 digits, as specified in the CHI standards")
-          }
-
-  ## Check that core identification variables are all present ----
-      for(var in c("indicator_key", "tab", "year", "cat1", "cat1_group",
-                   "source_date", "run_date")){
-        if(nrow(chi_est[is.na(get(var))]) > 0 ){
-          stop(glue::glue("There is at least one row where '{var}' is missing.
+  if(verbose){
+    message("Check that all observations have indicators, categories, tab, and year")
+  }
+  for(var in c("indicator_key", "tab", "year", "cat1", "cat1_group", "source_date", "run_date")){
+    if(nrow(chi_est[is.na(get(var))]) > 0 ){
+      status <- 0
+      if(verbose){
+        warning(glue::glue("There is at least one row where '{var}' is missing.
                           Please fill in the missing value before rerunning chi_qa()"))
-        }
       }
-
-      if(acs==F){
-        if(nrow(chi_est[is.na("cat1_varname")]) > 0 ){
-          stop(glue::glue("There is at least one row where 'cat1_varname' is missing.
+    }
+  }
+  if(acs==F){
+    if(nrow(chi_est[is.na("cat1_varname")]) > 0 ){
+      status <- 0
+      if(verbose){
+        warning(glue::glue("There is at least one row where 'cat1_varname' is missing.
                         Please fill in the missing value before rerunning chi_qa()"))
-        }
       }
-
-  ## Check that crosstab identification variables are all present ----
-      for(var in c("cat2", "cat2_group")){
-        if(nrow(chi_est[tab=="crosstabs" & is.na(get(var))]) > 0 ){
-          stop(glue::glue("There is at least one row where tab=='crosstabs' & where '{var}' is missing.
+    }
+  }
+  for(var in c("cat2", "cat2_group")){
+    if(nrow(chi_est[tab=="crosstabs" & is.na(get(var))]) > 0 ){
+      status <- 0
+      if(verbose){
+        warning(glue::glue("There is at least one row where tab=='crosstabs' & where '{var}' is missing.
                           Please fill in the missing value before rerunning chi_qa()"))
-        }
       }
-
-      if(acs==F){
-        if(nrow(chi_est[tab=="crosstabs" & is.na("cat2_varname")]) > 0 ){
-          stop(glue::glue("There is at least one row where 'cat2_varname' is missing.
+    }
+  }
+  if(acs==F){
+    if(nrow(chi_est[tab=="crosstabs" & is.na("cat2_varname")]) > 0 ){
+      status <- 0
+      if(verbose){
+        warning(glue::glue("There is at least one row where 'cat2_varname' is missing.
                       Please fill in the missing value before rerunning chi_qa()"))
-        }
       }
+    }
+  }
 
-  ## Check that results are always present if row is not suppressed ----
-      for(var in c("result", "lower_bound", "upper_bound", "se", "rse", "numerator", "denominator", "comparison_with_kc")){
-        if(nrow(chi_est[suppression != "^" & is.na(get(var))]) > 0 ){
-          stop(glue::glue("There is at least one row that is not suppressed & where '{var}' is missing.
+  if(verbose){
+    message("Checking that results are present if row is not suppressed")
+  }
+  for(var in c("result", "lower_bound", "upper_bound", "se", "rse", "numerator", "denominator", "comparison_with_kc")){
+    if(nrow(chi_est[suppression != "^" & is.na(get(var))]) > 0 ){
+      status <- 0
+      if(verbose){
+        warning(glue::glue("There is at least one row that is not suppressed & where '{var}' is missing.
                           Please fill in the missing value before rerunning chi_qa()"))
-        }
       }
+    }
+  }
 
-  ## Check that time_trends are always provided when tab=="trends" ----
-        if(ignore_trends == F){
-          if(nrow(chi_est[tab == "trends" & is.na(time_trends)]) > 0 ){
-            if(verbose) {
-              message(glue::glue("There is at least one row where tab=='trends' & where 'time_trends' is missing.
+  if(ignore_trends == F){
+    if(verbose){
+      message("Checking that time_trends are provided when tab=='trends'")
+    }
+    if(nrow(chi_est[tab == "trends" & is.na(time_trends)]) > 0 ){
+      status <- 0
+      if(verbose) {
+              warning(glue::glue("There is at least one row where tab=='trends' & where 'time_trends' is missing.
                             Please fill in the missing value before rerunning chi_qa()"))
-            } else {
-              return(0)
-            }
-          }
-        }
+      }
+    }
+  }
 
   ## Compare with previous year's results (FOR FUTURE???)----
       # in function arguments, have user submit most recent and comparison year(s). Submit as character b/c can be 2013-2017, not just 2017
@@ -360,14 +425,16 @@ chi_qa_tro <- function(chi_est = NULL, chi_meta = NULL, acs = F, ignore_trends =
       # actual comparison code should be the same as when comparing to a previous year, so write a small funcion to do this
 
   ## Print success statement!!!!!!!! ####
-    if(verbose) {
-      message(paste("\n\U0001f389Congratulations!\U0001f973\n",
-          "Your data has passed all CHI Tableau Ready formatting, style, and logic checks.\n",
-          "However, you are encouraged to read any warnings and consider whether you need to revise the data."))
+  if(verbose) {
+    if(status == 1){
+      message("Your data has passed all CHI Tableau Ready formatting, style, and logic checks.")
 
+    } else {
+      warning("At least one check has failed. Please review messages, make corrections, and rerun this check.")
     }
+  }
 
-    return(status)
+  return(status)
 
 }
 
