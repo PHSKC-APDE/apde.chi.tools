a45ac758e5077d300b4cbc1e1cc31caf2a5821cf
Danny Colombara <dcolombara@kingcounty.gov>
2025-02-10 11:14:37 -0800

    Commented out test for chi_generate_trend_years
    - chi_generate_trend_years does not exist for testing

--- tests/testthat/test-chi_process_trends.R
+++ tests/testthat/test-chi_process_trends.R
@@ -1,3 +1,4 @@
 test_that("calculates trends", {
-  DT <- chi_generate_trend_years(indicator_key = c("test1", "test2"),span = 3,begin.year = 2009,final.year = 2023)
+  # chi_generate_trend_years does not exist
+  # DT <- chi_generate_trend_years(indicator_key = c("test1", "test2"),span = 3,begin.year = 2009,final.year = 2023)
 })
