8a67eed34e7dd18ba00464faad4d7cb723b3a641
Buie <rbuie@kingcounty.gov>
2025-04-11 15:24:27 -0700

    added test for CHI generate estimates

--- tests/testthat/test-chi_generate_analysis_set.R
+++ tests/testthat/test-chi_generate_analysis_set.R
@@ -5,12 +5,10 @@ test_that("chi_generate_analysis_set validates inputs", {
   expect_error(chi_generate_analysis_set(CHIestimates = 123), "CHIestimates must be a data.table or data.frame")
 })
 
-test_that("chi_generate_analysis_set generates expected output", {
+test_that("chi_generate_analysis_set generates expected analysis set from CHI estimates", {
   TestData <- setup_test_data()
-  TestData$my.generic_data
-  TestData$my.analysis_set_twosets
-  test_analysis_instructions_results <- apde.chi.tools::chi_generate_tro_shell(TestData$my.analysis_set_twosets, end.year = 2023, year.span = 3, trend.span = 3, trend.periods = 5)
-  test_chi_calc_output <- apde.chi.tools::chi_calc(ph.data =  TestData$my.generic_data, ph.instructions = test_analysis_instructions_results, source_name =  "test",source_date = as.Date("2025-05-10"), ci = .95)
-
-
+  DT_test_analysis_instructions_results <- apde.chi.tools::chi_generate_tro_shell(TestData$my.analysis_set_twosets, end.year = 2023, year.span = 3, trend.span = 3, trend.periods = 5)
+  DT_test_chi_calc_output <- apde.chi.tools::chi_calc(ph.data =  TestData$my.generic_data, ph.instructions = DT_test_analysis_instructions_results, source_name =  "test",source_date = as.Date("2025-05-10"), ci = .95)
+  DT_recreated_analysis_set <- chi_generate_analysis_set(CHIestimates = DT_test_chi_calc_output)
+  expect_equal(all.equal(TestData$my.analysis_set_twosets[order(cat1,set)] , DT_recreated_analysis_set[order(cat1,set)]),TRUE)
 })
